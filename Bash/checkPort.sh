7afccebfd1173a8249a9bd9ab0619adf6415198b
Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>
2022-12-25 16:34:55 +0100

    ࿓❯ Update
    Signed-off-by: Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>

--- Bash/checkPort.sh
+++ Bash/checkPort.sh
@@ -5,7 +5,7 @@ if test "$#" -eq 2
 then
   HOST="$1"
   PORT="$2"
-  true 2>/dev/null>/dev/tcp/"$HOST"/"$PORT" && echo "open" || echo "closed"
+  timeout 2.0 bash -c "</dev/null>/dev/tcp/$HOST/$PORT" && echo "open" || echo "closed"
 else
   printf '%s\n%s\n%s\n' "Requires 2 arguments: [HOST] [PORT]" "[HOST]: Can be FQDN, IPv4 or IPv6" "[PORT]: Can be any valid port integer"
 fi
