6647f7d423ddacbea04cd30372cb804a8cd1238f
Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>
2022-12-27 16:18:14 +0100

    ࿓❯ Update
    Signed-off-by: Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>

--- Bash/updateGit.sh
+++ Bash/updateGit.sh
@@ -5,16 +5,17 @@ trap 'exit $?' EXIT ERR SIGTERM SIGABRT
 function updateGitRepository {
   local ADDIR MSG COMSG
   local -r GPG_KEY_ID="E2AC71651803A7F7"
-  local -r GIT_COMMIT_ARGS="( --signoff --gpg-sign=$GPG_KEY_ID -m $COMSG )"
+  
   if [[ "$#" -eq 1 ]] ; then
-  	ADDIR="$PWD"
-  	MSG="$1"
+    ADDIR="$PWD"
+    MSG="$1"
   elif [[ "$#" -gt 1 ]] ; then
-  	ADDIR="$1"
-  	MSG="${*:2}"
+    ADDIR="$1"
+    MSG="${*:2}"
   fi
   (
     COMSG="࿓❯ $MSG"
+    local -r GIT_COMMIT_ARGS=(--signoff --gpg-sign="$GPG_KEY_ID" -m "$COMSG")
     git add "$ADDIR"
     git commit "${GIT_COMMIT_ARGS[@]}"
     git push
