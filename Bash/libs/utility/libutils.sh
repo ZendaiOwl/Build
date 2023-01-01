2fcb1e212d72bb7aa7578220c93e189e4a056101
Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>
2023-01-01 15:14:42 +0100

    ࿓❯ Update
    Signed-off-by: Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>

--- Bash/libs/utility/libutils.sh
+++ Bash/libs/utility/libutils.sh
@@ -3,10 +3,17 @@
 #
 # Bash - Utility Functions
 #
-#
+
 # Check if user ID executing script/function is 0 or not
 isRoot() {
-  test "$EUID" -eq 0 && { echo "Is root" && return 0; } || { echo "Not root" && return 1; }
+  if test "$EUID" -eq 0
+  then
+    echo "Is root"
+    return 0
+  else
+    echo "Not root"
+    return 1
+  fi
 }
 
 # Gets the current time in UNIX & regular time (human-readable format)
@@ -68,18 +75,135 @@ showDirFiles() {
 
 # Search for a pattern recursively in files
 searchForPattern() {
-  PATTERN="$*"; grep --recursive --exclude-dir '.*' "$PATTERN" 2>/dev/null;
+  local -r PATTERN="$*"
+  grep --recursive --exclude-dir '.*' "$PATTERN" 2>/dev/null
 }
 
 # Search for a files with pattern recursively
 getFilesWithPattern() {
-  PATTERN="$*"; grep --files-with-matches --recursive --exclude-dir '.*' "$PATTERN" 2>/dev/null;
+  local -r PATTERN="$*"
+  grep --files-with-matches --recursive --exclude-dir '.*' "$PATTERN" 2>/dev/null
 }
 
+# Deletes a specified line in a file
+deleteLineInFile() {
+  if test "$#" -eq 2
+  then
+    local -r LINENR="$1" FILE="$2"
+    sed ''"$LINENR"'d' "$FILE"
+    return 0
+  else
+    echo "Requires 2 arguments: [Line number] [File]"
+    return 1
+  fi
+}
+
+# Deletes a specified range in a file
+deleteRangeInFile() {
+  if test "$#" -eq 3
+  then
+    local -r START="$1" END="$2" FILE="$3"
+    sed ''"$START"','"$END"'d' "$FILE"
+  else
+    echo "Requires 3 arguments: [Start of range] [End of range] [File]"
+    return 1
+  fi
+}
+
+# Replaces a text pattern in a file with new text
+replaceTextInFile() {
+  if test "$#" -eq 3
+  then
+    local -r FINDTEXT="$1" NEWTEXT="$2" FILE="$3"
+    sed "s|${FINDTEXT}|${NEWTEXT}|g" "$FILE"
+    return 0
+  else
+    echo "Requires 3 arguments: [Text to replace] [New text] [File]"
+    return 1
+  fi
+}
+
+# Appends text after line number
+appendTextAtLine() {
+  if test "$#" -eq 3
+  then
+    local -r LINENR="$1" TEXT="$2" FILE="$3"
+    sed ''"$LINENR"'a '"$TEXT"'' "$FILE"
+    return 0
+  else
+    echo "Requires 3 arguments: [Line number] [Text to append] [File]"
+    return 1
+  fi
+}
+
+# Appends text after matching text pattern
+appendTextAtPattern() {
+  if test "$#" -eq 3
+  then
+    local -r PATTERN="$1" TEXT="$2" FILE="$3"
+    sed '/'"$PATTERN"'/a '"$TEXT"'' "$FILE"
+    return 0
+  else
+    echo "Requires 3 arguments: [Text pattern] [Text to append] [File]"
+    return 1
+  fi
+}
+
+# Appends text after the last line
+appendTextAtLastLine() {
+  if test "$#" -eq 2
+  then
+    local -r TEXT="$1" FILE="$2"
+    sed '$a '"$TEXT"'' "$FILE"
+    return 0
+  else
+    echo "Requires 2 arguments: [Text to append] [File]"
+    return 1
+  fi
+}
+
+# Insert text before line number
+insertTextAtLine() {
+  if test "$#" -eq 3
+  then
+    local -r LINENR="$1" TEXT="$2" FILE="$3"
+    sed ''"$LINENR"'i '"$TEXT"'' "$FILE"
+    return 0
+  else
+    echo "Requires 3 arguments: [Line number] [Text to insert] [File]"
+    return 1
+  fi
+}
+
+# Insert text before matching text pattern
+insertTextAtPattern() {
+  if test "$#" -eq 3
+  then
+    local -r PATTERN="$1" TEXT="$2" FILE="$3"
+    sed '/'"$PATTERN"'/i '"$TEXT"'' "$FILE"
+    return 0
+  else
+    echo "Requires 3 arguments: [Text pattern] [Text to insert] [File]"
+    return 1
+  fi
+}
+
+# Inserts text before the last line
+insertTextAtLastLine() {
+  if test "$#" -eq 2
+  then
+    local -r TEXT="$1" FILE="$2"
+    sed '$i '"$TEXT"'' "$FILE"
+    return 0
+  else
+    echo "Requires 2 arguments: [Text to insert] [File]"
+    return 1
+  fi
+}
+
+# This function uses /dev/urandom to generate a password randomly.
+# Default length is 36
 genPassword() {
-  # This function uses /dev/urandom to generate a password randomly.
-  # Default length is 36
-  #
   # Ex. This one below uses the most commonly allowed password characters
   # < /dev/urandom tr -dc 'A-Z-a-z-0-9' | head -c${1:-32};echo;
   # 
@@ -94,19 +218,33 @@ genPassword() {
   # 8: 'A-Z a-z0-9{[|:?!#$@%+*^.~,=()/\\;]}'
   # 9: 'A-Z a-z 0-9 {[|:?!#$@%+*^.~,-()/;/=]}'
   # # # # # # # # # # # # # # # # # # # # # # #
-  < /dev/urandom tr -dc 'A-Z a-z0-9{[|:?!#$@%+*^.~,=()/\\;]}' | head -c"${1:-36}"; printf '\n';
+  #< /dev/urandom tr -dc 'A-Z a-z0-9{[|:?!#$@%+*^.~,=()/\\;]}' | head -c"${1:-36}"; printf '\n';
+  < /dev/urandom tr -dc 'A-Za-z0-9{[#$@]}' | head -c"${1:-36}"
+  printf '\n'
+  return 0
 }
 
+# Generates a password using OpenSSL, default length is 36.
 genOpenSSLPassword() {
-  # Generates a password using OpenSSL, default length is 36.
   openssl rand -base64 "${1:-36}"
+  return 0
 }
 
+# Gets the PATH for a script file
 getScriptPath() {
   test "$#" -eq 1 && {
-    PTH=$(type -p "$1"); file "$PTH";
+    PTH=$(type -p "$1")
+    file "$PTH"
+    return 0
   }
 }
+
+# TODO
+# Debug log function
+# Info log function
+# Success log function
+# Warning log function
+# Error log function
 
 # Displays 8 × 16-bit ANSI colours
 colour() {
@@ -119,10 +257,3 @@ colour() {
   done
   return 0
 }
-
-# TODO
-# Debug log function
-# Info log function
-# Success log function
-# Warning log function
-# Error log function
